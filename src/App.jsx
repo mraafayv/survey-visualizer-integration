1fbe46570def476f26f89bc5bdd5219a0bdcc8ac
Muhammad Raafay <raafayvohra.mrv@gmail.com>
2025-10-07 18:23:51 +0200

    define onChange event on CustomSelect to look for category changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import "./App.css";
 import useFetch from "./hooks/useFetch";
 import CategoriesChart from "./components/CategoriesChart";
 import CustomSelect from "./components/CustomSelect";
+import { useState } from "react";
 
 function App() {
+  const [category, setCategory] = useState(0);
   const {
     data: categoriesData,
     isPending: categoriesDataLoading,
@@ -16,12 +18,17 @@ function App() {
     error: categoriesLookupError,
   } = useFetch(`https://opentdb.com/api_category.php`);
 
+  const handleCategoryChange = (id) => {
+    setCategory(id);
+  };
+
   return (
     <>
       <h2>Survey Visualizer</h2>
       <CustomSelect
         label="Categories"
         options={categories?.trivia_categories}
+        handleChange={handleCategoryChange}
       />
       <div className="chart-container">
         <CategoriesChart data={categoriesData?.results} />
--- src/components/CustomSelect/CustomSelect.jsx
+++ src/components/CustomSelect/CustomSelect.jsx
@@ -1,12 +1,18 @@
 import "./CustomSelect.css";
 
-export const CustomSelect = ({ label, options = [] }) => {
+export const CustomSelect = ({ label, options = [], handleChange }) => {
   return (
     <div className="custom-select">
-      <label htmlFor={label} className="custom-label">{label}</label>
-      <select name={label} className="custom-dropdown">
+      <label htmlFor={label} className="custom-label">
+        {label}
+      </label>
+      <select
+        name={label}
+        className="custom-dropdown"
+        onChange={(e) => handleChange(e.target.value)}
+      >
         {options?.map((option) => (
-          <option key={option.id} value={option?.name}>
+          <option key={option.id} value={option?.id}>
             {option?.name}
           </option>
         ))}

75509cc6febd04ab1c3f3a5fbc902bbd63076484
Muhammad Raafay <raafayvohra.mrv@gmail.com>
2025-10-07 17:58:07 +0200

    use custom useFetch hook to fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
+import useFetch from "./hooks/useFetch";
 
 function App() {
+  const {
+    data: categoriesData,
+    isPending: categoriesDataLoading,
+    error: categoriesError,
+  } = useFetch(`https://opentdb.com/api.php?amount=50`);
 
-  return (
-    <h2>
-      Survey Visualizer
-    </h2>
-  );
+  return <h2>Survey Visualizer</h2>;
 }
 
 export default App;
